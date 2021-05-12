4ab5e79fa0dd4ea07d64268627f3d553c376576b
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-12 20:54:37 +0530

    Add User model

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+const db=require('./config/mongoose');
+app.use(express.urlencoded());
+app.use('/',require('./routes'));
+
 app.listen( port , ( err ) => {
     if( err ){
         console.log('Error in starting server')
