dd8aff9c55a120b328212d84dc1f1456c76800e9
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-14 17:17:14 +0530

    Add signup user

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,7 +1,34 @@
 const User = require('../../models/user/');
 
 module.exports.signupUser = async ( req , res ) => {
-    return res.status(200).json({
-        message : req.body
-    })
+    try{
+        const { email , username , password } = req.body;
+        let user = await User.findOne( { email : email } );
+        //console.log(user);
+        if( user ){
+            return res.status(400).json({
+                message : `Email <strong>${email}</strong> already exists`
+            })
+        }else{
+            user = await User.findOneAndRemove( { username : username } );
+            if( user ){
+                return res.status(400).json({
+                    message : `Username <strong>${username}</strong> already exists`
+                })
+            }else{
+                user = await User.create({
+                    email , username , password
+                });
+                return res.status(200).json({
+                    message : 'Account was created',
+                    user : { email : email , username : username }
+                })
+            }
+        }
+    }catch(error){
+        console.log(error)
+        return res.status(500).json({
+            message : 'Something went wrong'
+        })
+    }
 }
