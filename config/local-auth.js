41f34b9df238f0bc6c5f70491684e86acfaf48c4
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-14 20:40:46 +0530

    Add login

--- config/local-auth.js
+++ config/local-auth.js
@@ -4,12 +4,10 @@ module.exports.localAuth = async (req , res , next) => {
     try{
         const { email , password } = req.body;
         let user = await User.findOne({ email : email });
-        if( !user ){
-            user = await User.findOne({ username : email });
-        }
         if( user ){
             if( user.password === password ){
                 next();
+                return;
             }else{
                 return res.status(200).json({
                     message : 'Invalid username or password',
@@ -17,11 +15,25 @@ module.exports.localAuth = async (req , res , next) => {
                 }) 
             }
         }else{
-            return res.status(200).json({
-                message : 'Invalid username or password',
-                success : false
-            })
+            user = await User.findOne({ username : email });
+            if( user ){
+                if( user.password === password ){
+                    next();
+                    return;
+                }else{
+                    return res.status(200).json({
+                        message : 'Invalid username or password',
+                        success : false
+                    }) 
+                }
+            }else{
+                return res.status(200).json({
+                    message : 'Invalid username or password',
+                    success : false
+                })
+            }
         }
+        
     }catch(error){
         return res.status(500).json({
             message : 'Something went wrong'
--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,4 +1,5 @@
 const User = require('../../models/user/');
+const jwt = require('jsonwebtoken');
 
 module.exports.signupUser = async ( req , res ) => {
     try{
@@ -38,8 +39,34 @@ module.exports.signupUser = async ( req , res ) => {
     }
 }
 
-module.exports.login = (req , res) => {
-    return res.json(200).json({
-        message : "Logged in"
-    })
+module.exports.login = async (req , res) => {
+
+    try{
+        const { email } = req.body;
+        let user = await User.findOne({ email : email });
+        if( user ){
+            const token = await jwt.sign(user.toJSON() , 'curiosity' , { expiresIn : 1000000 } )
+            return res.status(200).json({
+                token : token ,
+                email : user.email ,
+                username : user.username,
+                success : true
+            })
+        }else{
+            user = await User.findOne({ username : email });
+            const token = await jwt.sign(user.toJSON() , 'curiosity' , { expiresIn : 1000000 } )
+            if( user ){
+                return res.status(200).json({
+                    token : token ,
+                    email : user.email ,
+                    username : user.username,
+                    success : true
+                })
+            }
+        }
+    }catch(error){
+        return res.status(500).json({
+            message : 'Something went wrong'
+        })
+    }  
 }
