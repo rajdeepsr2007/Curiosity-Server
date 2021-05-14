3ab6006f2de1d2f8032fb1dd4daa36cb06ef206f
rajdeepsr2007 <rajdeepsr2007@gmail.com>
2021-05-14 19:43:15 +0530

    Add local auth

---
+++ config/local-auth.js
@@ -0,0 +1,24 @@
+const User = require('../models/user/index');
+
+module.exports.localAuth = async (req , res , next) => {
+    try{
+        const { email , password } = req.body;
+        let user = await User.findOne({ email : email });
+        if( !user ){
+            user = await User.findOne({ username : email });
+        }
+        if( user ){
+            if( user.password === password ){
+                next();
+            }
+        }else{
+            return res.status(200).json({
+                message : 'Invalid username or password'
+            })
+        }
+    }catch(error){
+        return res.status(500).json({
+            message : 'Something went wrong'
+        })
+    }
+}
--- routes/api/auth/index.js
+++ routes/api/auth/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const authController = require('../../../controllers/auth/');
+const localAuth = require('../../../config/local-auth');
 
 router.post( '/signup' , authController.signupUser );
+router.post( '/login' , localAuth.localAuth , authController.login )
 
 module.exports = router;
